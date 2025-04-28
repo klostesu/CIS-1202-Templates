6d441828d0d3b15b2af47e58cbeec3a21d0422d0
Susan Klosterman <klostesu@gmail.com>
2025-04-28 16:32:24 -0400

    Add project files.

---
+++ CIS 1202 Templates.cpp
@@ -0,0 +1,22 @@
+// CIS 1202 Templates.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+
+}
+
+
+
