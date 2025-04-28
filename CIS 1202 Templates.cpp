2fa9eca5dcd9962e6c5a7eb5d733036063cda03d
Susan Klosterman <klostesu@gmail.com>
2025-04-28 17:04:28 -0400

    Fixed handling of int

--- CIS 1202 Templates.cpp
+++ CIS 1202 Templates.cpp
@@ -9,15 +9,16 @@ template <typename T>
 T half(T number) {
     return number / 2;
 }
+
 int half(int number) {
-    return static_cast<int>(number / 2.0 + 0.5);  // Add 0.5 to round up 
+    return static_cast<int>(static_cast<float>(number) / 2 + 0.5);
 }
 
 int main()
 {
-    double a = 7.0;
+    double a = 9.0;
     float b = 5.0f;
-    int c = 3;
+    int c = -8;
 
     cout << half(a) << endl;
     cout << half(b) << endl;
